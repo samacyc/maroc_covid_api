2dacdd2a679afbe5cbbb33aac063f036fef44a7c
=RedaElmar <=r.el.mar817991@gmail.com>
2020-05-24 16:11:10 +0000

    modified:   covid/settings.py 	modified:   runtime.txt

--- covid/settings.py
+++ covid/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '-rmvg5_tl5knpv0wcwzco)(5$$dsxu%cyxnty8il9m4c+7kh9b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
