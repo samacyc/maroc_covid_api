9ffa734fd585593b5613fb1958a008ae8a14a565
=RedaElmar <=r.el.mar817991@gmail.com>
2020-05-24 16:25:00 +0000

    edit whitenoise

--- covid/settings.py
+++ covid/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -129,4 +130,4 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
--- covid/wsgi.py
+++ covid/wsgi.py
@@ -1,9 +1,7 @@
 
 import os
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bootcamp.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "covid.settings")
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
